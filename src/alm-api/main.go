c34abe83794c4e9042db805fc5299f34f8637dce
yueshizhixin <yueshizhixin@163.com>
2018-07-20 23:23:22 +0800

    v0.0.1.2 session

--- src/alm-api/main.go
+++ src/alm-api/main.go
@@ -3,13 +3,38 @@ package main
 import (
 	"alm-api/global"
 	"github.com/gin-gonic/gin"
-	"alm-api/router"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-contrib/sessions"
 )
 
 func main() {
 	defer glb.DB.Close()
 	gin.SetMode(gin.DebugMode)
 	var r = gin.Default()
-	rout.InitRout(r)
+	store := cookie.NewStore([]byte("mysecret"))
+	r.Use(sessions.Sessions("mysession", store))
+	r.Use(sessions.Sessions("aaa", store))
+	r.GET("/test", func(c *gin.Context) {
+		session := sessions.Default(c)
+		var count int
+		v := session.Get("count")
+		if v == nil {
+			count = 0
+		} else {
+			count = v.(int)
+			count++
+		}
+		session.Set("count", count)
+		session.Save()
+		c.JSON(200, gin.H{"count": count})
+	})
+	r.GET("/test2", func(c *gin.Context) {
+		session := sessions.Default(c)
+		session.Clear()
+		session.Save()
+		c.JSON(200, gin.H{})
+	})
+	
+	//rout.InitRout(r)
 	r.Run(":80")
 }
--- src/alm-api/router/user.go
+++ src/alm-api/router/user.go
@@ -13,33 +13,24 @@ func userRout(r *gin.Engine) {
 	var err error
 
 	rg := r.Group("/user")
-	rg.POST("", func(ctx *gin.Context) {
-		var acc = strings.TrimSpace(ctx.PostForm("acc"))
-		var pwd = strings.TrimSpace(ctx.PostForm("pwd"))
-		var pwd2 = strings.TrimSpace(ctx.PostForm("pwd2"))
-		if pwd != pwd2 {
-			ctx.JSON(200, gin.H{
-				"code":"404",
-				"msg":"两次密码不同",
-				"data":"{}",
-			})
-			//return 
-		}
+	rg.GET("", func(ctx *gin.Context) {
+		//var acc = strings.TrimSpace(ctx.PostForm("acc"))
+		//var pwd = strings.TrimSpace(ctx.PostForm("pwd"))
 		var u = user.User{
-			Acc: acc,
-			Pwd: pwd,
+			Acc: strings.TrimSpace(ctx.DefaultQuery("acc", "")),
+			Pwd: strings.TrimSpace(ctx.DefaultQuery("pwd", "")),
 		}
 		if ok, err = svUser.Add(&u); ok {
 			ctx.JSON(200, gin.H{
-				"code":"200",
-				"msg":"操作成功",
-				"data":"{}",
+				"code": "200",
+				"msg":  "操作成功",
+				"data": "{}",
 			})
 			//return  
 		} else {
 			ctx.JSON(200, gin.H{
 				"code": "500",
-				"msg":  err,
+				"msg":  err.Error(),
 				"data": "{}",
 			})
 		}
